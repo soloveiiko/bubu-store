259f932fd380223c538142d6ca8c5d8dfc60302e
soloveiiko <soloveymaria51@gmail.com>
2023-08-06 14:06:45 +0300

    added: subscription style

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -5,17 +5,19 @@ import { FiArrowRight } from 'react-icons/fi';
 const Subscription = () => {
   return (
     <div className="subscription">
-      <div className="main-block">
-        <h2 className="headline">Отримуйте промокоди та ексклюзивні пропозиції</h2>
-        <div className="input-container">
-          <input type="email" placeholder="Ваш email" />
-          <button className="btn-container">
-            <FiArrowRight />
-          </button>
+      <div className="container">
+        <div className="main-block">
+          <h2 className="headline">Отримуйте промокоди та ексклюзивні пропозиції</h2>
+          <div className="input-container">
+            <input type="email" placeholder="Ваш email" />
+            <button className="btn-container">
+              <FiArrowRight />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="img-container">
-        <img src={footerImg} alt="Hello" />
+        <div className="img-container">
+          <img src={footerImg} alt="Hello" />
+        </div>
       </div>
     </div>
   );
