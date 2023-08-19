fe464a24a0a4d400acc45622f74e115eee5f2977
soloveiiko <soloveymaria51@gmail.com>
2023-08-19 18:30:48 +0300

    added: basket notification

--- src/components/commons/BasketNotification/BasketNotification.js
+++ src/components/commons/BasketNotification/BasketNotification.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BasketNotification = () => {
-  return <div>BasketNotification</div>;
+  return (
+    <div className="form-container">
+      <div className="basket-notification">
+        <h4 className="basket-notification__title">Товар додано до кошику</h4>
+        <button className="basket-notification__close-btn">Продовжити покупки</button>
+        <Link className="basket-notification__go-basket" to="#">
+          Перейти в кошик
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default BasketNotification;
--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -14,6 +14,7 @@ import {
   ProductCharacteristics,
   ProductReviews,
 } from '../../components';
+import BasketNotification from '../../components/commons/BasketNotification/BasketNotification';
 
 const ProductPage = () => {
   const [visibleReviews, setVisibleReviews] = useState(6);
@@ -76,6 +77,7 @@ const ProductPage = () => {
             </div>
             {recentlyViewedProducts.length > 0 && <BrowsingHistory recentlyViewedProducts={recentlyViewedProducts} />}
             <AddReview isOpenForm={isOpenForm} handleOpenForm={onOpenForm} />
+            <BasketNotification />
           </div>
         </div>
       ) : (
