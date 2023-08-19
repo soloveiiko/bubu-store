2a60b13f8a062d1d26ae739f4a03fffa83410cde
soloveiiko <soloveymaria51@gmail.com>
2023-08-19 12:34:26 +0300

    added: comments style

--- src/components/ProductReviews/ProductReviews.js
+++ src/components/ProductReviews/ProductReviews.js
@@ -4,7 +4,15 @@ import { AiFillStar } from 'react-icons/ai';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 const ProductReviews = ({ comments, visibleReviews, onShowMore }) => {
-  const breakpoints = { mobile: 0, tablet: 768, desktop: 1200 };
+  const getGap = () => {
+    if (window.innerWidth >= 768) {
+      return '30px';
+    } else if (window.innerWidth >= 360) {
+      return '16px';
+    } else {
+      return '0';
+    }
+  };
 
   return (
     <div className="product-reviews">
@@ -24,7 +32,7 @@ const ProductReviews = ({ comments, visibleReviews, onShowMore }) => {
           </div>
           <div className="comments-list">
             <ResponsiveMasonry columnsCountBreakPoints={{ 360: 1, 767: 2, 1199: 3 }}>
-              <Masonry className="comments-list-item">
+              <Masonry className="comments-list-item" gutter={getGap()}>
                 {comments.slice(0, visibleReviews).map((el) => (
                   <Review key={el.id} comment={el} />
                 ))}
